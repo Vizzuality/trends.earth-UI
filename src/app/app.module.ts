b3e045805b8f06314f7b9f1f35ad3debbd682602
rrequero <rrequero@gmail.com>
2017-04-17 13:12:57 +0200

    add github config

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,16 @@
+import { environment } from 'environments/environment';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule, RequestOptions } from '@angular/http';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MaterialRootModule } from '@angular/material';
+import {
+  LocationStrategy,
+  PathLocationStrategy,
+  HashLocationStrategy,
+  APP_BASE_HREF
+} from '@angular/common';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -13,6 +20,11 @@ import { LoginComponent } from "app/pages/login/login.component";
 import { OauthRequestOptions } from "app/services/oauth-request.service";
 import { AuthService, TokenService } from "app/services/auth.service";
 import { RecoverPasswordComponent } from "app/pages/recover-password/recover-password.component";
+
+let locationStrategy:any = PathLocationStrategy;
+if (environment.production) {
+  locationStrategy = HashLocationStrategy;
+}
 
 @NgModule({
   declarations: [
@@ -31,7 +43,9 @@ import { RecoverPasswordComponent } from "app/pages/recover-password/recover-pas
   providers: [
     TokenService,
     AuthService,
-    { provide: RequestOptions, useClass: OauthRequestOptions }
+    { provide: RequestOptions, useClass: OauthRequestOptions },
+    { provide: APP_BASE_HREF, useValue: '/' },
+    { provide: LocationStrategy, useClass: locationStrategy }
   ],
   bootstrap: [AppComponent]
 })
